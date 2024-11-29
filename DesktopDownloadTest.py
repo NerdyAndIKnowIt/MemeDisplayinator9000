cb590a6b0600929b76540e35d33e7cb90859c6bb
Breanna Chase <breannachase1@gmail.com>
2024-11-28 21:37:40 -0700

    v0.02 - authentication issue fixed, next step is downloading the images and building a test website on my personal computer

--- DesktopDownloadTest.py
+++ DesktopDownloadTest.py
@@ -1,32 +1,64 @@
-import credentials
+import AppCredentials
 import praw
+import requests
 
-
+# login using reddits api keys
 def login():
-     LoginCredentials = praw.Reddit(
-        #username = credentials.username,
-        #password = credentials.password,
-        client_id = credentials.client_id,
-        client_secret = credentials.client_secret,
-        user_agent = credentials.user_agent)
-     return LoginCredentials
+    print("Authenticating...")
+    reddit = praw.Reddit(
+        client_id = AppCredentials.client_id,
+        client_secret = AppCredentials.client_secret,
+        user_agent = AppCredentials.user_agent,
+        redirect_uri = AppCredentials.redirect_uri
+    )
+    print("Authenticated as {}".format(reddit.user.me()))
+    return reddit
 
+# retrieve the top posts of the month, limited to 10, given a subreddit
 def RetrievePosts(RedditCredentials, SubredditGroup):
-    for submission in RedditCredentials.subreddit(SubredditGroup).top(limit=10):
-        print(f"test")
-        if submission.url.endswith((".jpg", ".png")):
-            print(f"test")
-            print(f"Title: {submission.title}")
-            print(f"Image URL: {submission.url}")
+    TitleList = []
+    ImageURLList = []
 
+    print("These posts are from " + SubredditGroup)
+
+    for submission in RedditCredentials.subreddit(SubredditGroup).top(limit=10, time_filter="month"):
+        if submission.url.endswith((".jpg", ".png")):
+            print(f"Title: {submission.title}")
+            TitleVariable = SubredditGroup + ": " + submission.title
+            TitleList.append(TitleVariable)
+            print(f"Image URL: {submission.url}")
+            ImageURLList.append(submission.url)
+
+    return TitleList, ImageURLList
+
+
+# call the login function
 RedditCredentials = login()
 
-print(RedditCredentials.user.me())
+# call each subreddit with the retrieveposts function, and store the titles 
+SubredditGroup = "iiiiiiitttttttttttt"
+iiiiiiittttttttttttTitleList, iiiiiiittttttttttttURLList = RetrievePosts(RedditCredentials, SubredditGroup)
 
-#SubredditGroup = "iiiiiiitttttttttttt"
-#RetrievePosts(RedditCredentials, SubredditGroup)
+SubredditGroup = "ProgrammerHumor"
+ProgrammerHumorTitleList, ProgrammerHumorURLList = RetrievePosts(RedditCredentials, SubredditGroup)
 
-#SubredditGroup = "ProgrammerHumor"
-#RetrievePosts(RedditCredentials, SubredditGroup)
-#subreddit = reddit.subreddit("ProgrammerHumor")
+SubredditGroup = "ShittySysadmin"
+ShittySysadminTitleList, ShittySysadminURLList = RetrievePosts(RedditCredentials, SubredditGroup)
 
+SubredditGroup = "Sysadminhumor"
+SysadminhumorTitleList, SysadminhumorURLList = RetrievePosts(RedditCredentials, SubredditGroup)
+
+SubredditGroup = "windowsmemes"
+windowsmemesTitleList, windowsmemesURLList = RetrievePosts(RedditCredentials, SubredditGroup)
+
+SubredditGroup = "cablegore"
+cablegoreTitleList, cablegoreURLList = RetrievePosts(RedditCredentials, SubredditGroup)
+
+SubredditGroup = "networkingmemes"
+networkingmemesTitleList, networkingmemesURLList = RetrievePosts(RedditCredentials, SubredditGroup)
+
+SubredditGroup = "linuxmasterrace"
+linuxmasterraceTitleList, linuxmasterraceURLList = RetrievePosts(RedditCredentials, SubredditGroup)
+
+SubredditGroup = "programmingmemes"
+programmingmemesTitleList, programmingmemesURLList = RetrievePosts(RedditCredentials, SubredditGroup)
